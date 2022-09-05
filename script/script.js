5c4beee45e4e855c364a331d107c2cf820c0f67c
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-09-05 12:03:07 -0300

    05/09 Primeiro Commit

---
+++ script/script.js
@@ -0,0 +1,53 @@
+var touch = document.getElementById("val");
+
+function btn1(){
+    let b1 = 1
+    return b1
+}
+
+function btn2(){
+    let b2 = 2
+    return b2
+}
+
+function btn3(){
+    let b3 = 3
+    return b3
+}
+
+function btn4(){
+    let b4 = 4
+    return b4
+}
+
+function btn5(){
+    let b5 = 5
+    return b5
+}
+
+function btn6(){
+    let b6 = 6
+    return b6
+}
+
+function btn7(){
+    let b7 = 7
+    return b7
+}
+
+function btn8(){
+    let b8 = 8
+    return b8
+}
+
+function btn9(){
+    let b9 = 9
+    return b9
+}
+
+function btn0(){
+    let b0 = 0
+    return b0
+}
+
+touch.innerHTML = 0
