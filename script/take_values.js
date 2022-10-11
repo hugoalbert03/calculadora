4f83baf292d3541395be2610cdb9a3b1f6fe5dae
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-10-11 19:46:36 -0300

    09/10

---
+++ script/take_values.js
@@ -0,0 +1,38 @@
+var tela = document.getElementById("val").innerHTML = 0
+var v1 = undefined;
+var v2 = undefined;
+function btn1(){
+    
+}
+
+function btn2(){
+    
+}
+
+function btn3(){
+    
+}
+
+function btn4(){
+    
+}
+
+function btn5(){
+    
+}
+
+function btn6(){
+    
+}
+
+function btn7(){
+    
+}
+
+function btn8(){
+    
+}
+
+function btn9(){
+    
+}
