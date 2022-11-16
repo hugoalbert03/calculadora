6497b64ca36f480673c59d8e9f10e7ac69df09b2
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-11-15 23:37:59 -0300

    Commit 15/11/22
    - Começo de criação do desenvolvimento dos scripts
    
    - Melhoria na animação e estilização

--- script/aux_css.js
+++ script/aux_css.js
@@ -1,4 +1,25 @@
 let vs = window.document.querySelector("#visor") 
+let vw =  document.getElementById("vw")
+let botoes = document.getElementById("botoes")
+let visor = document.getElementById("vcamp")
 vs.onclick = function(){
     
+}
+function view(){
+    let desl = 0
+    if(vw.value == "Esconder Teclas ▲"){
+        desl = "12rem"
+        vw.value = "Mostrar Teclas ▼"
+        vw.style.background = "green"
+        vw.style.color = "white"
+        botoes.style.display = "none"
+    }else{
+        vw.value = "Esconder Teclas ▲"
+        desl = "0"
+        vw.style.background = "red"
+        vw.style.color = "white"
+        botoes.style.display = ""
+    }
+    visor.style["margin-top"] = `${desl}`
+    visor.style.transition = "0.5s"
 }
--- script/take_values.js
+++ script/take_values.js
@@ -1,42 +1,48 @@
-var tela = document.getElementById("visor").innerHTML = 0
+var tela = document.getElementById("visor").innerHTML;
+
+let clear = document.getElementById("clear")
 var v1 = undefined;
 var v2 = undefined;
-function btn1(){
 
+function btn0(){
+    document.getElementById("visor").innerHTML += 0
+}
+
+function btn1(){
+    document.getElementById("visor").innerHTML += 1
 }
 
 function btn2(){
-    
+    document.getElementById("visor").innerHTML += 2
 }
 
 function btn3(){
-    
+    document.getElementById("visor").innerHTML += 3   
 }
 
 function btn4(){
-    
+    document.getElementById("visor").innerHTML += 4
 }
 
 function btn5(){
-    
+    document.getElementById("visor").innerHTML += 5
 }
 
 function btn6(){
-    
+    document.getElementById("visor").innerHTML += 6
 }
 
 function btn7(){
-    
+    document.getElementById("visor").innerHTML += 7
 }
 
 function btn8(){
-    
+    document.getElementById("visor").innerHTML += 8
 }
 
 function btn9(){
-    
+    document.getElementById("visor").innerHTML += 9
 }
 
 function confirm(){
-
 }
