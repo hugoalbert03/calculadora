ae94584b6a5fed5fab8db1d28cc094228be3cd77
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-11-25 14:13:02 -0300

    Commit Pendente
    25/11/2022

--- script/aux_css.js
+++ script/aux_css.js
@@ -1,20 +1,22 @@
-let vs = window.document.querySelector("#visor") 
 let vw =  document.getElementById("vw")
 let botoes = document.getElementById("botoes")
 let visor = document.getElementById("vcamp")
+vw.style.background = "red"
+vw.style.color = "white"
+
 vs.onclick = function(){
     
 }
 function view(){
     let desl = 0
-    if(vw.value == "Esconder Teclas ▲"){
+    if(vw.value == "Modo Digitação"){
         desl = "12rem"
-        vw.value = "Mostrar Teclas ▼"
+        vw.value = "Modo Calculadora"
         vw.style.background = "green"
         vw.style.color = "white"
         botoes.style.display = "none"
     }else{
-        vw.value = "Esconder Teclas ▲"
+        vw.value = "Modo Digitação"
         desl = "0"
         vw.style.background = "red"
         vw.style.color = "white"
